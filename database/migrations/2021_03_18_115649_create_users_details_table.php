7fb0d0dccacba606f0c5d569dc11b0677e8b22af
ridhoajibx <ridhoajibx@gmail.com>
2021-03-18 19:06:18 +0700

    [ridho] - make migration table & add column

---
+++ database/migrations/2021_03_18_115649_create_users_details_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersDetailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users_details', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
+            $table->string('address');
+            $table->enum('gender', ['male', 'female'])->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users_details');
+    }
+}
